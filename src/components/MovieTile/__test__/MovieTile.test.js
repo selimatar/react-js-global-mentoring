3a2ea4859ee8a8ee8e4d99c1e1a8af1c22d5c51e
s.atar <s.atar@emakina.com.tr>
2023-05-24 16:14:15 +0300

    other tests were fixed

--- src/components/MovieTile/__test__/MovieTile.test.js
+++ src/components/MovieTile/__test__/MovieTile.test.js
@@ -1,16 +1,24 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import MovieTile from "../movieTile";
-import { movieList } from "../../../data/movies";
 
 describe("MovieTile", () => {
   const movie = {
-    imageUrl: movieList[3].poster_path,
-    title: movieList[3].title,
-    releaseYear: movieList[3].release_date,
-    genres: movieList[3].genres,
-    description: movieList[3].overview,
-    duration: movieList[3].runtime,
-    rating: movieList[3].vote_average,
+      "id": 424785,
+      "title": "Transformers 7",
+      "tagline": "",
+      "vote_average": 0,
+      "vote_count": 0,
+      "release_date": "2019-06-26",
+      "poster_path": "https://image.tmdb.org/t/p/w500/432BowXw7a4fWXSONxBaFKqvW4f.jpg",
+      "overview": "Plot unknown.",
+      "budget": 0,
+      "revenue": 0,
+      "genres": [
+          "Science Fiction",
+          "Action",
+          "Adventure"
+      ],
+      "runtime": null
   };
   
   test("renders movie tile component", () => {
